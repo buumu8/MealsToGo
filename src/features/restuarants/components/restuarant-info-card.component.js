37770c957c6fe75ec9d6d8159650ee6c955ec6aa
Buumu8 <buumu8@gmail.com>
2022-03-16 17:30:32 +0700

    Add Address

--- src/features/restuarants/components/restuarant-info-card.component.js
+++ src/features/restuarants/components/restuarant-info-card.component.js
@@ -3,8 +3,15 @@ import styled from "styled-components/native";
 import { Card } from "react-native-paper";
 
 const Title = styled.Text`
-  padding-top: 16px;
+  font-size: ${(props) => props.theme.fontSizes.body};
+  font-family: ${(props) => props.theme.fonts.heading};
+  color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Address = styled.Text`
+  font-size: ${(props) => props.theme.fontSizes.caption};
   font-family: ${(props) => props.theme.fonts.body};
+  color: ${(props) => props.theme.colors.ui.primary};
 `;
 
 const RestaurantCard = styled(Card)`
@@ -14,6 +21,10 @@ const RestaurantCard = styled(Card)`
 
 const RestaurantCardCover = styled(Card.Cover)`
   background-color: ${(props) => props.theme.colors.bg.primary};
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Info = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
@@ -33,7 +44,10 @@ export default function RestaurantsInfoCard({ restuarant = {} }) {
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 }
